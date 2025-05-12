139f8987192cb5552ba227ca269e3ccb8becc427
zhongwy <merpokcasterl@yandex.ru>
2025-05-12 22:20:47 +0300

    Исправлены пути импортов и конфиг Vite

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import FaqView from '@/views/FaqView.vue'
-import DocsView from '@/views/DocsView.vue'
-import HomePageView from '@/views/HomePageView.vue'
+import FaqView from '../views/FaqView.vue'
+import DocsView from '../views/DocsView.vue'
+import HomePageView from '../views/HomePageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,36 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueJsx from '@vitejs/plugin-vue-jsx'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [vue(), vueJsx(), vueDevTools()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url)),
+//     },
+//   },
+//   base: '/',
+// })
+
+import { fileURLToPath, URL } from 'node:url'
+
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~': fileURLToPath(new URL('./node_modules', import.meta.url)),
+    },
+    extensions: ['.js', '.ts', '.vue', '.json'],
+  },
+  base: '/0x00001234/',
+  build: {
+    rollupOptions: {
+      external: [], // Явно указываем внешние зависимости, если нужно
     },
   },
-  base: '/',
 })
